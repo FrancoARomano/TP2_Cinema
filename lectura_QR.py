f0b73908794030e721df1d08daf41360818abad7
Franco Romano <franromano03@gmail.com>
2023-12-07 17:40:52 -0300

    con main

--- lectura_QR.py
+++ lectura_QR.py
@@ -4,74 +4,78 @@ from pyzbar import pyzbar
 from PIL import Image
 import os
 
-centinela_1 = True
 
-centinela_2 = True
+def main() -> None: 
+    centinela_1 = True
 
-while centinela_1:
-    
-    decision = input("Si quiere ingresar el id del QR ingrese 0, Si quiere leer el QR con su Webcam ingrese 1: ")
+    centinela_2 = True
 
-    if decision == "0" or decision == "1":
-        centinela_1 = False
-
-if decision == "0":
-    """
-    tiene que haber un qr existente para que pueda ser ingresado
-    """
-    centinela_3 = True
-    
-    while centinela_3:
+    while centinela_1:
         
-        id_ingresado = input("Ingrese el id de su QR ")
-        
-        if os.path.exists(f"ID_{id_ingresado}.png"):
-            
-            imagen = Image.open(f"ID_{id_ingresado}.png")
-            codigo_qr = pyzbar.decode(imagen)[0]
-            
-            info_del_qr = codigo_qr.data.decode("utf8")
-            
-            with open("ingresos.txt", "a") as ingresos_txt:
-            
-                ingresos_txt.write(info_del_qr + "\n")
-            
-            ingresos_txt.close()
-            
-            centinela_3 = False
+        decision = input("Si quiere ingresar el id del QR ingrese 0, Si quiere leer el QR con su Webcam ingrese 1: ")
 
-elif decision == "1":
-    
-    cap = cv2.VideoCapture(0)
-    
-    print("Para salir presione q con la ventana de la cámara")
-    
-    while centinela_2:
+        if decision == "0" or decision == "1":
+            centinela_1 = False
+
+    if decision == "0":
+        """
+        tiene que haber un qr existente para que pueda ser ingresado
+        """
+        centinela_3 = True
         
-        ret, frame = cap.read()
+        while centinela_3:
             
-        for codes in decode(frame):
-            info = codes.data.decode('utf-8')
+            id_ingresado = input("Ingrese el id de su QR ")
             
-            with open("ingresos.txt", "a") as ingresos_txt:
-            
-                ingresos_txt.write(info + "\n")
+            if os.path.exists(f"ID_{id_ingresado}.png"):
                 
-            with open("ingresos.txt", "r") as archivo:
-                contenido = archivo.read()
-                contenido_lista = contenido.split("\n")
+                imagen = Image.open(f"ID_{id_ingresado}.png")
+                codigo_qr = pyzbar.decode(imagen)[0]
                 
-                for i in contenido_lista:
-                    if info == i: centinela_2 = False
-            
-            ingresos_txt.close()
-        
-        cv2.imshow("LECTOR DE QR", frame)
-            
-        t = cv2.waitKey(1)
-        
-        if t == ord('q'):
-            centinela_2 = False
+                info_del_qr = codigo_qr.data.decode("utf8")
+                
+                with open("ingresos.txt", "a") as ingresos_txt:
+                
+                    ingresos_txt.write(info_del_qr + "\n")
+                
+                ingresos_txt.close()
+                
+                centinela_3 = False
 
-    cv2.destroyAllWindows()
-    cap.release()
+    elif decision == "1":
+        
+        cap = cv2.VideoCapture(0)
+        
+        print("Para salir presione q con la ventana de la cámara")
+        
+        while centinela_2:
+            
+            ret, frame = cap.read()
+                
+            for codes in decode(frame):
+                info = codes.data.decode('utf-8')
+                
+                with open("ingresos.txt", "a") as ingresos_txt:
+                
+                    ingresos_txt.write(info + "\n")
+                    
+                with open("ingresos.txt", "r") as archivo:
+                    contenido = archivo.read()
+                    contenido_lista = contenido.split("\n")
+                    
+                    for i in contenido_lista:
+                        if info == i: centinela_2 = False
+                
+                ingresos_txt.close()
+            
+            cv2.imshow("LECTOR DE QR", frame)
+                
+            t = cv2.waitKey(1)
+            
+            if t == ord('q'):
+                centinela_2 = False
+
+        cv2.destroyAllWindows()
+        cap.release()
+        
+main()
